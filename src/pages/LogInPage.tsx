29654c02d48673e4f27f2c5a392bf49bbe61f1be
dheeraj-Joshi0208 <dheerajjoshi0208@gmail.com>
2025-07-31 20:51:49 +0530

    Create login form.

--- src/pages/LogInPage.tsx
+++ src/pages/LogInPage.tsx
@@ -1,3 +1,11 @@
+import { LoginForm } from "@/components/login-form";
+
 export const LogInPage = () => {
-  return <div>LogInPage</div>;
+  return (
+    <div className="flex min-h-screen w-full items-center justify-center p-6">
+      <div className="w-full max-w-sm">
+        <LoginForm />
+      </div>
+    </div>
+  );
 };
